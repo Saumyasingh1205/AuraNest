10a88917ce6cf052229098175c93ca8eeb9d2ba2
Saumya Singh <saumyaisngh5223@gmail.com>
2025-12-30 15:54:43 +0530

    Updated checkout and order history

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get("/api/products", (req, res) => {
 // ----------------------
 // USERS (AUTH)
 // ----------------------
-const usersPath = path.join(__dirname, "users.json");
+const usersPath = path.join(__dirname,"data", "users.json");
 
 const readUsers = () => {
   if (!fs.existsSync(usersPath)) return [];
